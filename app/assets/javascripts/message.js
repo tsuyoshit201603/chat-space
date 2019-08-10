f632b3bbd31e8e2f65f87b2c244cff5dab54a85d
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-08-10 15:58:47 +0900

    define reloadMessages in app/assets/javascripts/message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,24 @@
 $(function(){
   function buildHTML(message){
+    // 条件分岐が壊れてる
     var content = message.content ? `${message.content}` : "";
     var image = message.image ? `<img class="lower-message__image" src=${message.image}>` : "";
-    var html = `<div class="body__right__content">
-                    <div class="body__right__content__user">
-                      ${message.user_name}
-                    </div>
-                    <div class="body__right__content__date">
-                      ${message.date}
-                    </div>
+    var html = `<div class="message__box" data-id=${message.id}>
+                  <div class="body__right__content">
+                      <div class="body__right__content__user">
+                        ${message.user_name}
+                      </div>
+                      <div class="body__right__content__date">
+                        ${message.date}
+                      </div>
                   </div>
                   <div class="body__right__message">
-                    <p class="lower-message__content">
-                      ${content}
-                    </p>
-                    ${image}
-                  </div>`
+                      <p class="lower-message__content">
+                        ${content}
+                      </p>
+                      ${image}
+                  </div>
+                </div>`
     return html;
   }
   
@@ -44,4 +47,25 @@ $(function(){
       $('.body__right__footer__send').prop( 'disabled', false )
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message__box').last().data('id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: '/groups/last_message_id/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
 })
